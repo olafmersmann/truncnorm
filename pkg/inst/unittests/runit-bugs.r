d085d8c4ff4afb50891958c01f3c8082a4849eca
Olaf Mersmann <olafm@statistik.tu-dortmund.de>
2011-03-22 14:30:03 +0100

    Fix bugs in accept-reject sampler for rtruncnorm().
    Bug originally reported by Sune Karlsson with a partial fix. Further
    investigation showed that more edge cases needed to be changed in the
    selection heuristic for the sampling strategy in case 'a' and 'b' are
    finite.
    
    Additionally some debugging aids were added to the C source code.
    These are not active by default but can be turned on at compile time
    by defining the DEBUG directive.
    
    Finally the 'urs_a_b' sampler was marginally simplifed.

--- pkg/inst/unittests/runit-bugs.r
+++ pkg/inst/unittests/runit-bugs.r
@@ -15,30 +15,56 @@ test.002 <- function() {
   checkEquals(length(data), length(1:10))
 }
 
+check_rtruncnorm <- function(a, b, mean, sd, n=2e7) {
+  x <- rtruncnorm(n=n, a, b, mean, sd)
+  mx <- mean(x)
+  vx <- var(x)
+  ev <- etruncnorm(a, b, mean, sd)
+  vv <- vtruncnorm(a, b, mean, sd)
+  checkTrue(all(x >= a))
+  checkTrue(all(x <= b))
+  checkEqualsNumeric(ev, mx, tolerance=1e-3)
+  checkEqualsNumeric(vv, vx, tolerance=1e-3)
+}
+
 ## These two regression tests are not checked by default because the
 ## are stochastic in nature and might fail even though everything is
 ## OK. If you change anything in rtruncnorm.c, be sure to run these
 ## and investigate if they fail.
-if (FALSE) {
+if (TRUE) {  
   ## Wrong sampling strategy:
   test.003 <- function() {
-    x <- rtruncnorm(n=10000000, -0.75, 0.75, 0, 1)
-    mx <- mean(x)
-    vx <- var(x)
-    ev <- etruncnorm(-0.75, 0.75, 0, 1)
-    vv <- vtruncnorm(-0.75, 0.75, 0, 1)
-    checkEqualsNumeric(ev, mx, tolerance=1e-3)
-    checkEqualsNumeric(vv, vx, tolerance=1e-3)
+    set.seed(42)
+    check_rtruncnorm(-0.75, 0.75, 0, 1)
   }
   
   ## Wrong sampling strategy:
   test.004 <- function() {
-    x <- rtruncnorm(n=10000000, -1, 4, 0, 1)
-    mx <- mean(x)
-    vx <- var(x)
-    ev <- etruncnorm(-1, 4, 0, 1)
-    vv <- vtruncnorm(-1, 4, 0, 1)
-    checkEqualsNumeric(ev, mx, tolerance=1e-3)
-    checkEqualsNumeric(vv, vx, tolerance=1e-3)
+    set.seed(42)
+    check_rtruncnorm(-1, 4, 0, 1) ## Case 2 (a)
+  }
+
+  test.005 <- function() {
+    set.seed(42)
+    check_rtruncnorm(0.5, 1.5, 6.5, 0.8) ## Case 3s (b)
+  }
+  
+  ## Magic constants for reference:
+  ##
+  ##   t1 = 0.15, t2 = 2.18, t3 = 0.725, t4 = 0.45
+  ## 
+  test.006 <- function() {
+    set.seed(42)
+    check_rtruncnorm(-3, -2, 0, 1) # 3s 
+  }
+
+  test.007 <- function() {
+    set.seed(42)
+    check_rtruncnorm(-2, -1, 0, 1)
+  }
+
+  test.008 <- function() {
+    set.seed(42)
+    check_rtruncnorm(1, 2, 0, 1)
   }
 }
--- pkg/src/rtruncnorm.c
+++ pkg/src/rtruncnorm.c
@@ -31,6 +31,12 @@
 #include <R_ext/BLAS.h>
 #include <R_ext/Applic.h>
 
+#ifdef DEBUG
+#define SAMPLER_DEBUG(N, A, B) Rprintf("%8s(%f, %f)\n", N, A, B)
+#else
+#define SAMPLER_DEBUG(N, A, B) 
+#endif
+
 static const double t1 = 0.15;
 static const double t2 = 2.18;
 static const double t3 = 0.725;
@@ -38,8 +44,9 @@ static const double t4 = 0.45;
 
 /* Exponential rejection sampling (a,inf) */
 static R_INLINE double ers_a_inf(double a) {
+    SAMPLER_DEBUG("ers_a_inf", a, R_PosInf);
     const double ainv = 1.0 / a;
-    double x,rho;
+    double x, rho;
     do{
 	x = rexp(ainv) + a; /* rexp works with 1/lambda */
 	rho = exp(-0.5 * pow((x - a), 2));
@@ -49,6 +56,7 @@ static R_INLINE double ers_a_inf(double a) {
 
 /* Exponential rejection sampling (a,b) */
 static R_INLINE double ers_a_b(double a, double b) {
+    SAMPLER_DEBUG("ers_a_b", a, b);
     const double ainv = 1.0 / a;
     double x, rho;
     do{
@@ -60,46 +68,46 @@ static R_INLINE double ers_a_b(double a, double b) {
 
 /* Normal rejection sampling (a,b) */
 static R_INLINE double nrs_a_b(double a, double b){
-  double x = a - 1.0;
-  while(x < a || x > b){
-    x = rnorm(0, 1);
-  }
-  return x;
+    SAMPLER_DEBUG("nrs_a_b", a, b);
+    double x = a - 1.0;
+    while(x < a || x > b){
+        x = rnorm(0, 1);
+    }
+    return x;
 }
 
 /* Normal rejection sampling (a,inf) */
 static R_INLINE double nrs_a_inf(double a){
-  double x = a - 1.0;
-  while(x < a){
-    x = rnorm(0, 1);
-  }
-  return x;
+    SAMPLER_DEBUG("nrs_a_inf", a, R_PosInf);
+    double x = a - 1.0;
+    while(x < a){
+        x = rnorm(0, 1);
+    }
+    return x;
 }
 
 /* Half-normal rejection sampling */
 double hnrs_a_b(double a, double b){
-  double x = a - 1.0;
-  while(x < a || x > b) {
-    x = rnorm(0, 1);
-    x = fabs(x);
-  }
-  return x;
+    SAMPLER_DEBUG("hnrs_a_b", a, b);    
+    double x = a - 1.0;
+    while(x < a || x > b) {
+        x = rnorm(0, 1);
+        x = fabs(x);
+    }
+    return x;
 }
 
 /* Uniform rejection sampling */
 static R_INLINE double urs_a_b(double a, double b){
+    SAMPLER_DEBUG("urs_a_b", a, b);
     const double phi_a = dnorm(a, 0.0, 1.0, FALSE);
     double x = 0.0, u = 0.0;
-    if (a < 0 && b > 0) {
-	do{
-	    x = runif(a, b);
-	} while (runif(0, 1) * M_1_SQRT_2PI > dnorm(x,0,1,0));
-    } else {
-	do{
-	    x = runif(a, b);
-	    u = runif(0, 1);
-	} while (runif(0, 1) * phi_a > dnorm(x, 0.0, 1.0, FALSE));
-    }
+    
+    /* Upper bound of normal density on [a, b] */
+    const double ub = a < 0 && b > 0 ? M_1_SQRT_2PI : phi_a;
+    do{
+        x = runif(a, b);
+    } while (runif(0, 1) * ub > dnorm(x,0,1,0));
     return x;
 }
 
@@ -122,33 +130,33 @@ static R_INLINE double r_righttruncnorm(double b, double mean, double sd) {
 static R_INLINE double r_truncnorm(double a, double b, double mean, double sd) {
     const double alpha = (a - mean) / sd;
     const double beta = (b - mean) / sd;
-    const double phi_a = dnorm(a, 0.0, 1.0, FALSE);
-    const double phi_b = dnorm(b, 0.0, 1.0, FALSE);
+    const double phi_a = dnorm(alpha, 0.0, 1.0, FALSE);
+    const double phi_b = dnorm(beta, 0.0, 1.0, FALSE);
     if (beta <= alpha) {
 	return NA_REAL;
-    } else if (alpha < 0 && 0 < beta) {
-	if (phi_a < t1 && phi_b < t1) {
+    } else if (alpha <= 0 && 0 <= beta) { /* 2 */
+	if (phi_a <= t1 || phi_b <= t1) { /* 2 (a) */
 	    return mean + sd * nrs_a_b(alpha, beta);
-	} else {
+	} else { /* 2 (b) */
 	    return mean + sd * urs_a_b(alpha, beta);
 	}
-    } else if (alpha >= 0) {
-	if (phi_a / phi_b <= t2) {
+    } else if (alpha > 0) { /* 3 */
+	if (phi_a / phi_b <= t2) { /* 3 (a) */
 	    return mean + sd * urs_a_b(alpha, beta);
 	} else {
-	    if (alpha < t3) {
+	    if (alpha < t3) { /* 3 (b) */                
 		return mean + sd * hnrs_a_b(alpha, beta);
-	    } else {
+	    } else { /* 3 (c) */
 		return mean + sd * ers_a_b(alpha, beta);
 	    }
 	}
-    } else { /* beta <= 0 */
-	if (phi_a / phi_b <= t2) {
+    } else { /* 3s */
+	if (phi_b / phi_a <= t2) { /* 3s (a) */
 	    return mean - sd * urs_a_b(-beta, -alpha);
 	} else {
-	    if (alpha < t3) {
+	    if (beta > -t3) { /* 3s (b) */
 		return mean - sd * hnrs_a_b(-beta, -alpha);
-	    } else {
+	    } else { /* 3s (c) */
 		return mean - sd * ers_a_b(-beta, -alpha);
 	    }
 	}
@@ -156,41 +164,41 @@ static R_INLINE double r_truncnorm(double a, double b, double mean, double sd) {
 }
 
 SEXP do_rtruncnorm(SEXP s_n, SEXP s_a, SEXP s_b, SEXP s_mean, SEXP s_sd) {
-  R_len_t i, nn;
-  UNPACK_INT(s_n, n);
-  if (NA_INTEGER == n)
-      error("n is NA - aborting.");
-  UNPACK_REAL_VECTOR(s_a   , a   , n_a);
-  UNPACK_REAL_VECTOR(s_b   , b   , n_b);
-  UNPACK_REAL_VECTOR(s_mean, mean, n_mean);
-  UNPACK_REAL_VECTOR(s_sd  , sd  , n_sd);
-
-  nn = MAX(n, MAX(MAX(n_a, n_b), MAX(n_mean, n_sd)));
-  ALLOC_REAL_VECTOR(s_ret, ret, nn);
-
-  GetRNGstate();
-  for (i = 0; i < nn; ++i) {
-    const double ca = a[i % n_a];
-    const double cb = b[i % n_b];
-    const double cmean = mean[i % n_mean];
-    const double csd = sd[i % n_sd];
-
-    if (R_FINITE(ca) && R_FINITE(cb)) {
-	ret[i] = r_truncnorm(ca, cb, cmean, csd);
-    } else if (R_NegInf == ca && R_FINITE(cb)) {
-	ret[i] = r_righttruncnorm(cb, cmean, csd);
-    } else if (R_FINITE(ca) && R_PosInf == cb) {
-	ret[i] = r_lefttruncnorm(ca, cmean, csd);
-    } else if (R_NegInf == ca && R_PosInf == cb) {
-	ret[i] = rnorm(cmean, csd);
-    } else {
-	ret[i] = NA_REAL;
+    R_len_t i, nn;
+    UNPACK_INT(s_n, n);
+    if (NA_INTEGER == n)
+        error("n is NA - aborting.");
+    UNPACK_REAL_VECTOR(s_a   , a   , n_a);
+    UNPACK_REAL_VECTOR(s_b   , b   , n_b);
+    UNPACK_REAL_VECTOR(s_mean, mean, n_mean);
+    UNPACK_REAL_VECTOR(s_sd  , sd  , n_sd);
+    
+    nn = MAX(n, MAX(MAX(n_a, n_b), MAX(n_mean, n_sd)));
+    ALLOC_REAL_VECTOR(s_ret, ret, nn);
+    
+    GetRNGstate();
+    for (i = 0; i < nn; ++i) {
+        const double ca = a[i % n_a];
+        const double cb = b[i % n_b];
+        const double cmean = mean[i % n_mean];
+        const double csd = sd[i % n_sd];
+        
+        if (R_FINITE(ca) && R_FINITE(cb)) {
+            ret[i] = r_truncnorm(ca, cb, cmean, csd);
+        } else if (R_NegInf == ca && R_FINITE(cb)) {
+            ret[i] = r_righttruncnorm(cb, cmean, csd);
+        } else if (R_FINITE(ca) && R_PosInf == cb) {
+            ret[i] = r_lefttruncnorm(ca, cmean, csd);
+        } else if (R_NegInf == ca && R_PosInf == cb) {
+            ret[i] = rnorm(cmean, csd);
+        } else {
+            ret[i] = NA_REAL;
+        }
+        R_CheckUserInterrupt();
     }
-    R_CheckUserInterrupt();
-  }
-  PutRNGstate();
-  UNPROTECT(1); /* s_ret */
-  return s_ret;
+    PutRNGstate();
+    UNPROTECT(1); /* s_ret */
+    return s_ret;
 }
 
 
